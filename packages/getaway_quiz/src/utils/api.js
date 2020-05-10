74876010f08269200cafbba2000740cb46aeb366
Stuart Lynn <stuart.lynn@gmail.com>
2020-05-10 10:55:17 -0400

    making sure we are pointing at the right server

--- packages/getaway_quiz/src/utils/api.js
+++ packages/getaway_quiz/src/utils/api.js
@@ -1,5 +1,6 @@
 
-const API_URL = 'http://localhost:5000'
+// const API_URL = 'http://localhost:5000'
+const API_URL = process.env.NODE_ENV ==='production' ? 'https://famquiz.herokuapp.com' : 'http://localhost:5000'
 
 export const postAPI = (endpoint, body)=>{
     console.log("POSITNG to ", API_URL+'/getaway/'+endpoint )
