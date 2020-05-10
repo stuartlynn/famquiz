72545186bf3ad7bf8ed57469be41bdeb0bf85b42
Stuart Lynn <stuart.lynn@gmail.com>
2020-05-10 10:24:00 -0400

    Updates

---
+++ precache-manifest.48248d5b7d2b10f2ae89aaa94e738f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657550ecd86c62c00575877c31eb5b85",
+    "url": "/famquiz/index.html"
+  },
+  {
+    "revision": "7f32c83c6abed96dedc4",
+    "url": "/famquiz/static/css/main.5e198081.chunk.css"
+  },
+  {
+    "revision": "73638e9bd30e80a8c276",
+    "url": "/famquiz/static/js/2.5e9769ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/famquiz/static/js/2.5e9769ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f32c83c6abed96dedc4",
+    "url": "/famquiz/static/js/main.80c85497.chunk.js"
+  },
+  {
+    "revision": "295c4847e9fd998a9a8c",
+    "url": "/famquiz/static/js/runtime-main.080dcecc.js"
+  }
+]);
